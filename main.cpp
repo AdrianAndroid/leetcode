9c05f76848ed2fdf38727129ad92d3a7d175f41f
zhaojian2 <zhaojian2@joyy.com>
2022-01-26 14:07:22 +0800

    方法一：按层模拟

--- main.cpp
+++ main.cpp
@@ -14,34 +14,34 @@
 using namespace std;
 
 class Solution {
-private:
-    static constexpr int directions[4][2] = {{0,  1},
-                                             {1,  0},
-                                             {0,  -1},
-                                             {-1, 0}};
 public:
-    vector<int> spiralOrder(vector<vector<int>> &matrix) {
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
         if (matrix.size() == 0 || matrix[0].size() == 0) {
             return {};
         }
 
         int rows = matrix.size(), columns = matrix[0].size();
-        vector<vector<bool>> visited(rows, vector<bool>(columns));
-        int total = rows * columns;
-        vector<int> order(total);
-
-        int row = 0, column = 0;
-        int directionIndex = 0;
-        for (int i = 0; i < total; i++) {
-            order[i] = matrix[row][column];
-            visited[row][column] = true;
-            int nextRow = row + directions[directionIndex][0], nextColumn = column + directions[directionIndex][1];
-            if (nextRow < 0 || nextRow >= rows || nextColumn < 0 || nextColumn >= columns ||
-                visited[nextRow][nextColumn]) {
-                directionIndex = (directionIndex + 1) % 4;
+        vector<int> order;
+        int left = 0, right = columns - 1, top = 0, bottom = rows - 1;
+        while (left <= right && top <= bottom) {
+            for (int column = left; column <= right; column++) {
+                order.push_back(matrix[top][column]);
             }
-            row += directions[directionIndex][0];
-            column += directions[directionIndex][1];
+            for (int row = top + 1; row <= bottom; row++) {
+                order.push_back(matrix[row][right]);
+            }
+            if (left < right && top < bottom) {
+                for (int column = right - 1; column > left; column--) {
+                    order.push_back(matrix[bottom][column]);
+                }
+                for (int row = bottom; row > top; row--) {
+                    order.push_back(matrix[row][left]);
+                }
+            }
+            left++;
+            right--;
+            top++;
+            bottom--;
         }
         return order;
     }
@@ -60,5 +60,7 @@ int main() {
     }
     // 1 , 2 , 3 , 6 , 9 , 8 , 7 , 4 , 5 ,
     //[1 , 2 , 3 , 6 , 9 , 8 , 7 , 4 , 5 ]
+    cout << "" << endl;
+    cout << "1 , 2 , 3 , 6 , 9 , 8 , 7 , 4 , 5" << endl;
     return 0;
 }
