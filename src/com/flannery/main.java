df16c48482c5e269b11ec675f9eb453821d1f133
zhaojian2 <zhaojian2@joyy.com>
2022-03-30 13:52:27 +0800

    synchronized

--- src/com/flannery/main.java
+++ src/com/flannery/main.java
@@ -1,9 +1,18 @@
 package com.flannery;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Lock lock = new ReentrantLock();
+        lock.lock();
+        try {
+            System.out.println("Hello World!!");
+        } finally {
+            lock.unlock();
+        }
     }
 
 }
