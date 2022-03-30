02a6a095933139f044800470c915d807421f6ae0
zhaojian2 <zhaojian2@joyy.com>
2022-03-30 09:48:50 +0800

    synchronized

--- src/com/flannery/main.java
+++ src/com/flannery/main.java
@@ -6,4 +6,20 @@ public class main {
         System.out.println("Hello World!");
     }
 
+    // 修饰实例方法
+    public synchronized void increase1() {
+        //i++;
+    }
+
+    // 修饰静态方法
+    public static synchronized void increase2() {
+        //i++;
+    }
+
+    // 修饰代码块
+    public void synMethod(Object a1) {
+        synchronized (a1) { // 需要一个实例
+
+        }
+    }
 }
