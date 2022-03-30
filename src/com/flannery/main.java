a151804d872b58df4fe133e9e747d2c4a0aea8f9
zhaojian2 <zhaojian2@joyy.com>
2022-03-30 15:07:18 +0800

    ConcurrentHashMap

--- src/com/flannery/main.java
+++ src/com/flannery/main.java
@@ -1,9 +1,12 @@
 package com.flannery;
 
+import java.util.concurrent.ConcurrentHashMap;
+
 public class main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        ConcurrentHashMap<String, String> map = new ConcurrentHashMap<>();
     }
 
 }
