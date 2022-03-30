f5f12ce2c8caeaaf08e7bf36deb4802ac5f537fd
zhaojian2 <zhaojian2@joyy.com>
2022-03-30 15:04:59 +0800

    HashMap

--- src/com/flannery/main.java
+++ src/com/flannery/main.java
@@ -1,9 +1,19 @@
 package com.flannery;
 
+import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        HashMap<String, String> map = new HashMap<>();
+        map.put("", "");
+        System.out.println(Integer.toBinaryString("asdfasdf".hashCode() ^ "asdfasdf".hashCode()>>>16));
+        // (key == null) ? 0 : (h = key.hashCode()) ^ (h >>> 16)
+        // 1101000000101110 0110111001101000
+        //                  1101000000101110
+        // 1101000000101110 1011111001000110
     }
 
 }
