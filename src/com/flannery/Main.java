ed8c50ab41f23ac764b2fea7a48788f14b60377e
zhaojian2 <zhaojian2@joyy.sg>
2022-04-07 16:19:05 +0800

    动态代理深究

--- src/com/flannery/Main.java
+++ src/com/flannery/Main.java
@@ -3,10 +3,16 @@ package com.flannery;
 import java.util.concurrent.*;
 
 // https://blog.csdn.net/qq_40177006/article/details/113006868
+// 问题：
+// 只有一个核心线程， 但该核心线程被阻塞
+//
+// 解决：
+// 1.将LinkedBlockingQueue换成有限队列ArrayBlockingQueue（按照测试代码，可以将队列设置容量小于3）
+// 2.将核心线程数扩大（只要保证核心线程不全部被阻塞即可）
 public class Main {
 
     private static ThreadPoolExecutor sThreadPool =
-            new ThreadPoolExecutor(1, 10, 1, TimeUnit.MINUTES, new LinkedBlockingQueue<Runnable>());
+            new ThreadPoolExecutor(1, 10, 1, TimeUnit.MINUTES, new LinkedBlockingQueue<Runnable>(3));
 
     private static void execute(Runnable runnable) {
         try {
@@ -40,13 +46,21 @@ public class Main {
             public void run() {
                 System.out.println("countDown in");
                 countDownLatch.countDown();
+                System.out.println(""+countDownLatch.getCount());
                 System.out.println("countDown out");
             }
         };
 
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 7; i++) {
             execute(countDownRunnable);
         }
+
+        execute(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("normal");
+            }
+        });
     }
 
     public static void main(String[] args) {
