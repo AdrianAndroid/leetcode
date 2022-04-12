15fd725a1f1499d602a8e9f4867bed655154b40c
zhaojian2 <zhaojian2@joyy.sg>
2022-04-12 19:01:59 +0800

    ThreadLocal

--- src/com/flannery/main.java
+++ src/com/flannery/main.java
@@ -4,6 +4,25 @@ public class main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        ThreadLocal<String> t1 = new ThreadLocal<>();
+        t1.set("Hello");
+
+        new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                System.out.println("tttt "+t1.get());
+                t1.set("World");
+                System.out.println("tttt "+t1.get());
+            }
+        }.start();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("xxxx "+t1.get());
     }
 
 }
