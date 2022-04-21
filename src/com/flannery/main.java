2c1d10a080922e6b75d18d80eaa13faaec8e35c9
zhaojian <Flannery.zhao@kikatech.com>
2022-04-21 22:51:13 +0800

    myPow

--- src/com/flannery/main.java
+++ src/com/flannery/main.java
@@ -6,4 +6,20 @@ public class main {
         System.out.println("Hello World!");
     }
 
+
+    public static double myPow(double x, int n) {
+        if (x == 0.0f) return 0.0d;
+        long b = n;
+        double res = 1.0;
+        if (b < 0) {
+            x = 1 / x;
+            b = -b;
+        }
+        while (b > 0) {
+            if ((b & 1) == 1) res *= x;
+            x *= x;
+            b >>= 1;
+        }
+        return res;
+    }
 }
