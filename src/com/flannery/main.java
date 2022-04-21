9a60458dc83d490e08be78b93775e642d345f18e
zhaojian <Flannery.zhao@kikatech.com>
2022-04-21 23:43:55 +0800

    dfs

--- src/com/flannery/main.java
+++ src/com/flannery/main.java
@@ -6,4 +6,31 @@ public class main {
         System.out.println("Hello World!");
     }
 
+
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) return 0;
+        int nums = 0;
+        // 循环
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    nums++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+        return nums;
+    }
+
+    private void dfs(char[][] grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == '0') { // 判断边界
+            return; // 不符合要求
+        }
+        grid[i][j] = '0'; // 染色
+        // 左上右下
+        dfs(grid, i - 1, j);
+        dfs(grid, i + 1, j);
+        dfs(grid, i, j - 1);
+        dfs(grid, i, j + 1);
+    }
 }
