8d004feafdbd60e3a113317fe945d80a0e56b013
zhaojian <Flannery.zhao@kikatech.com>
2022-04-22 17:57:20 +0800

    Set差集

--- src/com/flannery/main.java
+++ src/com/flannery/main.java
@@ -1,9 +1,28 @@
 package com.flannery;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        Set<String> set1 = new HashSet<>();
+        set1.add("1");
+        set1.add("2");
+        set1.add("3");
+        set1.add("4");
+        set1.add("5");
+
+        Set<String> set2 = new HashSet<>();
+        set2.add("1");
+        set2.add("2");
+        set2.add("3");
+
+        set1.removeAll(set2);
+
+        System.out.println(set1);
+
     }
 
 }
