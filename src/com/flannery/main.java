2f9bf203074444547831ba3a229057f3b8f681c9
zhaojian <Flannery.zhao@kikatech.com>
2022-04-22 15:16:33 +0800

    LinkedList

--- src/com/flannery/main.java
+++ src/com/flannery/main.java
@@ -1,9 +1,14 @@
 package com.flannery;
 
+import java.util.LinkedList;
+
 public class main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        LinkedList<Character> stack = new LinkedList<>();
+        Character ch = stack.removeLast();
+        System.out.println(ch);
     }
 
 }
