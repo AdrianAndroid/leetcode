986d423052ed5883d997b747d72f7bfffd0548d0
zhaojian <Flannery.zhao@kikatech.com>
2022-04-26 11:36:51 +0800

    HashMap

--- src/com/flannery/main.java
+++ src/com/flannery/main.java
@@ -1,6 +1,7 @@
 package com.flannery;
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class main {
@@ -8,7 +9,15 @@ public class main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         HashMap<String, String> map = new HashMap<>();
-        map.put("", "");
+        map.put("111", "111");
+        map.put("222", "111");
+        map.put("333", "111");
+        map.put("444", "111");
+
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " = " + entry.getValue());
+        }
+
         System.out.println(Integer.toBinaryString("asdfasdf".hashCode() ^ "asdfasdf".hashCode()>>>16));
         // (key == null) ? 0 : (h = key.hashCode()) ^ (h >>> 16)
         // 1101000000101110 0110111001101000
